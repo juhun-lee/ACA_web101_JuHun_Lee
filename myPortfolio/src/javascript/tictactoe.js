6391a740e09df188cdf102153fa8689651b1810a
juhun-lee <juhunlee@bu.edu>
2019-02-27 09:54:45 -0600

    Created TicTacToe Page

---
+++ myPortfolio/src/javascript/tictactoe.js
@@ -0,0 +1,49 @@
+console.log("My javascript is working!")
+
+var marker = "O"
+var marker = "X"
+
+function place(topleft) {
+    document.getElementById(topleft).innerHTML = marker = "O";
+}
+
+function place(topcenter) {
+    document.getElementById(topcenter).innerHTML = "O";
+}
+
+function place(topright) {
+    document.getElementById(topright).innerHTML = "O";
+}
+
+function place(middleleft) {
+    document.getElementById(middleleft).innerHTML = "O";
+}
+
+function place(middlecenter) {
+    document.getElementById(middlecenter).innerHTML = "O";
+}
+
+function place(bottomleft) {
+    document.getElementById(bottomleft).innerHTML = "O";
+}
+
+function place(bottomcenter) {
+    document.getElementById(bottomcenter).innerHTML = "O";
+}
+
+function place(bottomright) {
+    document.getElementById(bottomright).innerHTML = "O";
+}
+
+if (marker = "O") {
+    document.getElementById(topleft).innerHTML = marker = "X";
+}
+
+// function changeMarkerToX(topleft){ 
+//     document.getElementById(topleft).innerHTML = "X";
+
+// }
+
+// function changeMarkerToO(){ 
+
+// }
