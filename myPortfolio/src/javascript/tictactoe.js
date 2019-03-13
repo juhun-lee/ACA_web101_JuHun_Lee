ba97ce1f117373da700706e0b7f9d346726e6216
juhun-lee <juhunlee@bu.edu>
2019-03-13 04:01:38 -0500

    Update Tictactoe page

--- myPortfolio/src/javascript/tictactoe.js
+++ myPortfolio/src/javascript/tictactoe.js
@@ -1,49 +1,20 @@
+// Checking if javascript is working through Console.log //
 console.log("My javascript is working!")
 
-var marker = "O"
-var marker = "X"
+// Define variables //
+var gameMarker = "X";  
 
-function place(topleft) {
-    document.getElementById(topleft).innerHTML = marker = "O";
-}
+// Creating onclick function //
+function place(this_id){
+     document.getElementById(this_id).innerHTML = gameMarker;
 
-function place(topcenter) {
-    document.getElementById(topcenter).innerHTML = "O";
-}
+// These codes will change the mark to the other one, if it is X, it will be O. 
+addEventListener("click", place);
+ if (gameMarker === 'X') {
+      gameMarker = 'O'
+    } else {
+      gameMarker = 'X'
+    }
+ }
 
-function place(topright) {
-    document.getElementById(topright).innerHTML = "O";
-}
-
-function place(middleleft) {
-    document.getElementById(middleleft).innerHTML = "O";
-}
-
-function place(middlecenter) {
-    document.getElementById(middlecenter).innerHTML = "O";
-}
-
-function place(bottomleft) {
-    document.getElementById(bottomleft).innerHTML = "O";
-}
-
-function place(bottomcenter) {
-    document.getElementById(bottomcenter).innerHTML = "O";
-}
-
-function place(bottomright) {
-    document.getElementById(bottomright).innerHTML = "O";
-}
-
-if (marker = "O") {
-    document.getElementById(topleft).innerHTML = marker = "X";
-}
-
-// function changeMarkerToX(topleft){ 
-//     document.getElementById(topleft).innerHTML = "X";
-
-// }
-
-// function changeMarkerToO(){ 
-
-// }
+ // The problems I faced were I could not make boxes freeze after the first click. I figured that it happens because of function conflicts.
